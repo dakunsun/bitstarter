6b39237f8c3153cbbcbc25ff9985fe0cc5d0132d
Steve Sun <dakun.sun@gmail.com>
2013-07-10 03:55:58 +0000

    modified web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   fs = require('fs');
-  var buffer = new Buffer(fs.readFileSync("index.html"));
+  var buffer = fs.readFileSync("index.html");
   response.send(buffer.toString()); 
 });
 
